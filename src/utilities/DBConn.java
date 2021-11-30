8b3db595012844893b4f61e5fc7b0a98200dd109
Josh-presentTense <200464522@student.georgianc.on.ca>
2021-11-30 01:27:59 -0500

    Initial commit

---
+++ src/utilities/DBConn.java
@@ -0,0 +1,9 @@
+package utilities;
+
+public class DBConn {
+    // ADD CONNECTION INFO TO RUN
+    private static String user = "root";
+    private static String password = "Josh@9891";
+    private static String connString = "jdbc:mysql://localhost:3306/labex05db";
+
+}

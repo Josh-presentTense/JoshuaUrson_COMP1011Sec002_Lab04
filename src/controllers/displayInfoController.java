5337472e8c3f908d1ef527238815126f652de8a3
Josh-presentTense <200464522@student.georgianc.on.ca>
2021-12-03 22:12:43 -0500

    Added interface to the displayInfoView.fxml. Added ids and initializable method, tested dbconnection to get games and players info.

--- src/controllers/displayInfoController.java
+++ src/controllers/displayInfoController.java
@@ -1,4 +1,47 @@
 package controllers;
 
-public class displayInfoController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import models.Game;
+import models.Player;
+import models.PlayerAndGame;
+import utilities.DBConn;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class displayInfoController implements Initializable {
+
+    @FXML
+    private ListView<Game> gamesListView;
+
+    @FXML
+    private ListView<Player> playersListView;
+
+    @FXML
+    private ListView<PlayerAndGame> recordsListView;
+
+    @FXML
+    private ComboBox<Integer> playerIdComboBox;
+
+    @FXML
+    private ComboBox<Integer> gameIdComboBox;
+
+    @FXML
+    private TextArea msgTextArea;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        try {
+            gamesListView.getItems().addAll(DBConn.getGamesFromDB());
+            playersListView.getItems().addAll(DBConn.getPlayersFromDB());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
